14665aa233bebfb7117e2fa5995b58dccd9fbf86
Aravind Esampelly <126321224+explorewitharavind@users.noreply.github.com>
2023-04-23 15:51:21 +0530

    Add files via upload

---
+++ MethodsinJava.java
@@ -0,0 +1,12 @@
+public class MethodsinJava {
+    public static void Addition()
+    {
+        int x=2;
+        int y=3;
+        System.out.println(x+y);
+    }
+    public static void main(String[] args) {
+        Addition();
+    }
+    
+}
