afd7bd1566828fe3018ed4370495d844257902cb
Aravind Esampelly <126321224+explorewitharavind@users.noreply.github.com>
2023-04-24 12:29:59 +0530

    Add files via upload

---
+++ MathClass.java
@@ -0,0 +1,8 @@
+public class MathClass {
+   public static void main(String[] args) {
+     int a =10;
+     int b =20;
+     int Addition = Math.max(a,b);
+     System.out.println(Addition);
+   }
+}
