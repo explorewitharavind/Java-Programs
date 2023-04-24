5536757cacab957bd4ed53caacf4fd0d428ddc99
Aravind Esampelly <126321224+explorewitharavind@users.noreply.github.com>
2023-04-24 20:12:15 +0530

    Add files via upload

---
+++ MethodParameters.java
@@ -0,0 +1,10 @@
+public class MethodParameters
+{
+    static void Addition(int a,int b)
+    {
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        Addition(10, 20);
+    }
+}
