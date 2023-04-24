181523bf16feb95ae93e588112332ff01ff13ecf
Aravind Esampelly <126321224+explorewitharavind@users.noreply.github.com>
2023-04-24 12:36:17 +0530

    Add files via upload

---
+++ MathClassSquaroot.java
@@ -0,0 +1,8 @@
+public class MathClassSquaroot {
+    public static void main(String[] args) {
+        
+        int b =20;
+        double Squaroot =  Math.sqrt(b);
+        System.out.println("Squaroot value is "+ Squaroot);
+      }
+}
