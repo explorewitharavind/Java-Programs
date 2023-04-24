50a368991b79eea0fe5c6cf057e7e472c2e98dbc
Aravind Esampelly <126321224+explorewitharavind@users.noreply.github.com>
2023-04-24 12:32:36 +0530

    Add files via upload

---
+++ MathClassMinumum.java
@@ -0,0 +1,8 @@
+public class MathClassMinumum {
+    public static void main(String[] args) {
+        int a =10;
+        int b =20;
+        int Minumum = Math.min(a,b);
+        System.out.println("Minimum Value is "+ Minumum);
+      }
+}
