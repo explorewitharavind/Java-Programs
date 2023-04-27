148437d48029f80b1a73eca653695697d65a4268
Aravind Esampelly <126321224+explorewitharavind@users.noreply.github.com>
2023-04-27 10:50:07 +0530

    Add files via upload

---
+++ ArrayOneDimensional.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ArrayOneDimensional
+{
+    public static void main(String[] args) {
+        int[]  Array = {0,1,2,3,4};
+       
+        for(int i=0;i<Array.length;i++)
+        {
+           
+            System.out.println(Array[i]);
+        }
+
+    }
+}
