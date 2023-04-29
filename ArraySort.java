841f92440e6b6cb7ff213e253e9a6cdb2ca30d45
Aravind Esampelly <126321224+explorewitharavind@users.noreply.github.com>
2023-04-29 18:02:00 +0530

    Add files via upload

---
+++ ArraySort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        int myArray[] = {1,2,4,6,7,5};
+        Arrays.sort(myArray);
+        System.out.println(Arrays.toString(myArray));
+    }
+}
