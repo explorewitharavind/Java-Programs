4da40aae79ed97b9030d10fd9e54dc8788a586f6
Aravind Esampelly <126321224+explorewitharavind@users.noreply.github.com>
2023-05-01 12:40:21 +0530

    Add files via upload

---
+++ Addition/Sum.java
@@ -0,0 +1,14 @@
+package Addition;
+
+public class Sum {
+
+    public void Display()
+    {
+        int a =2;
+        int b =3;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
+
+
---
+++ CheckingPackage.java
@@ -0,0 +1,7 @@
+import Addition.Sum;
+public class CheckingPackage {
+    public static void main(String[] args) {
+        Sum ob = new Sum();
+        ob.Display();
+    }
+}
